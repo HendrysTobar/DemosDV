61c13ec36841035ab1320426ba96e2930582e3dc
HendrysTobar <fabiantobar@gmail.com>
2019-02-15 18:06:07 -0500

    :new: demo Rotaciones de Robot 2018/02/15

---
+++ Demo Rotaciones/Assets/RobotMovimiento.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMovimiento : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Moverse());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator Moverse()
+    {
+        //Mientras que la posición en x sea menor a 0
+        while(this.transform.position.x < 0)
+        {
+            //Moverse Hacia la derecha
+            transform.Translate(transform.right * 3 * Time.deltaTime, Space.World);
+            yield return null;
+        }
+
+        //Toma la decisión de moverse arriba o abajo.
+
+        int r = Random.Range(0, 2);
+        //0 = abajo, 1 = arriba
+        if(r == 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -45f);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 45f);
+        }
+
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        transform.localScale *= 2;
+        while(sr.isVisible)
+        {
+            transform.Translate(Vector3.right * 3 * Time.deltaTime, Space.Self);
+            yield return null;
+        }
+
+        Destroy(this.gameObject);
+
+
+    }
+
+}
---
+++ Demo Rotaciones/Assets/RobotMovimientoGiro.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMovimientoGiro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Si se presiona Left Arrow
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Apuntar al Oeste
+            //this.transform.rotation = Quaternion.AngleAxis(180f, Vector3.up);
+            //Esto es equivalente
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.right, Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Apuntar al Oeste
+            //this.transform.rotation = Quaternion.AngleAxis(0f, Vector3.up);
+            //Esto es equivalente
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.right, Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Apuntar al Oeste
+            //this.transform.rotation = Quaternion.AngleAxis(90f, Vector3.forward);
+            //Esto es equivalente
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.right, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Apuntar al Oeste
+            //this.transform.rotation = Quaternion.AngleAxis(-90f, Vector3.forward);
+            //Esto es equivalente
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.right, Vector3.down);
+           
+        }
+
+        transform.Translate(Vector3.right * Time.deltaTime);		
+	}
+}
